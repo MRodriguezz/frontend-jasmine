338009cd76ad6f74133a4998982ec17bf4f54bba
Sebastian Ibarguen <s@cognits.co>
2015-01-20 11:55:52 -0600

    Added new functions and tests.

--- spec/spec.js
+++ spec/spec.js
@@ -4,4 +4,21 @@ describe("First jasmine project", function() {
     expect(helloWorld()).toBe("hello world");
   });
 
+  it("should write the title of the page", function(){
+    expect(titleText).toBe("Jasmine project. Cognits!");
+    expect(titleText).not.toBe("");
+  });
+
+  it("should include in the title jasmine", function(){
+    expect(titleText).toMatch(/[jJ]asmine/);
+  });
+
+  it("should be able to add", function(){
+    expect(suma(2, 2)).toBe(4);
+    expect(suma(3, 6)).toBe(9);
+    expect(suma(-50, 1000)).toBe(950);
+    expect(suma("50", "400")).toBe(450);
+    expect(suma("this is a string", 20)).toBe(null);
+  });
+
 });
--- src/script.js
+++ src/script.js
@@ -1,3 +1,30 @@
+var titleText = "Jasmine project. Cognits!";
+var listOfItems = Array();
+
+
 function helloWorld() {
   return "hello world";
 }
+
+
+function title() {
+  document.write("<h1>" + titleText + "</h1>");
+}
+title();
+
+function writeFooterOfPage() {
+    document.write("<p> This is: " + titleText + "</p>");
+}
+
+
+function suma(x, y) {
+
+  x = parseFloat(x);
+  y = parseFloat(y);
+
+  if( isNaN(x)  || isNaN(y) ) {
+    return null;
+  }
+  return x + y;
+}
+suma("this should now work",5);
